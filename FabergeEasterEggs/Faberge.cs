f6969aaf2eaeb4d89e9027bf85143f109f3ce608
Albert Soczewski <albert.soczewski@gmail.com>
2018-09-16 17:28:42 +0200

    Adds FabergeEasterEggs kata

---
+++ FabergeEasterEggs/Faberge.cs
@@ -0,0 +1,48 @@
+﻿using System.Numerics;
+
+namespace FabergeEasterEggs
+{
+    public static class Faberge
+    {
+        public static BigInteger Height(int eggs, int tries, BigInteger[,] memory = null)
+        {
+            if (memory == null)
+            {
+                memory = new BigInteger[eggs + 1, tries + 1];
+            }
+
+            if (memory[eggs, tries] != default(BigInteger))
+            {
+                return memory[eggs, tries];
+            }
+
+            if (eggs == 0 || tries == 0)
+            {
+                memory[eggs, tries] = 0;
+                return 0;
+            }
+
+            if (eggs == 1)
+            {
+                memory[eggs, tries] = tries;
+                return tries;
+            }
+
+            if (eggs == 2)
+            {
+                //Basic case, see http://datagenetics.com/blog/july22012/index.html
+                var height = tries * (tries + 1) / 2;
+                memory[eggs, tries] = height;
+                return height;
+            }
+
+            var eggBrokenCase = Height(eggs - 1, tries - 1, memory);
+            memory[eggs - 1, tries - 1] = eggBrokenCase;
+
+            var eggSurvivedCase = Height(eggs, tries - 1, memory);
+            memory[eggs, tries - 1] = eggSurvivedCase;
+
+            return 1 + eggBrokenCase + eggSurvivedCase;
+        }
+    }
+}
